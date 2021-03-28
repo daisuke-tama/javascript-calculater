04e97e32901ded68308adcd65c4029dec5078b6e
EC2 Default User <ec2-user@ip-172-31-12-161.us-east-2.compute.internal>
2021-03-28 06:10:13 +0000

    演算子修正

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-　　let target = document.getElementById("stop")
+　　let stop = document.getElementById('push_symbol');
+　　let stop2 = document.getElementById('push_symbol2');
+　　let stop3 = document.getElementById('push_symbol3');
+　　let stop4 = document.getElementById('push_symbol4');
+　　let stop5 = document.getElementById('push_symbol5');
 　　
+　　function symbol_push() {
+　    　stop.disabled = true;
+        stop2.disabled = true;
+        stop3.disabled = true;
+        stop4.disabled = true;
+        stop5.disabled = true;
+　　};
 　　
-    function update( _v )
+　　function symbol_not_push() {
+        stop.disabled = false;
+        stop2.disabled = false;
+        stop3.disabled = false;
+        stop4.disabled = false;
+        stop5.disabled = false;
+　　};
+　　
+　　function update( _v )
     {
-        document.querySelector( 'input' ).value = _v
+        document.querySelector( 'input' ).value = _v;
+        symbol_not_push()
     }
     
     function symbol( _v )
     {
-        document.querySelector( 'input' ).value += _v
-        target.disabled = true
-
+        document.querySelector( 'input' ).value += _v;
+        symbol_push()
     }
     
     function append( _v )
     {
-        document.querySelector( 'input' ).value += _v
-        target.disabled = false
+        document.querySelector( 'input' ).value += _v;
+        symbol_not_push()
     }
 
     function calc()
@@ -29,4 +48,3 @@
             update( _error )
         }
     }
-    
